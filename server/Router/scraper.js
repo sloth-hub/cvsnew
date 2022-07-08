02dae7379d7b974ce63d0b97b1b01c24975c5229
sloth-hub <madamhh@gmail.com>
2022-07-08 21:34:55 +0900

    Fix: CU 스크래핑 코드 수정

--- server/Router/scraper.js
+++ server/Router/scraper.js
@@ -5,26 +5,30 @@ const puppeteer = require("puppeteer");
 async function scrapData() {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto("https://cu.bgfretail.com/product/product.do?category=product&depth2=4&sf=N");
-    await page.waitForSelector("#dataTable > div.prodListBtn > div.prodListBtn-w > a");
-    await page.click("#dataTable > div.prodListBtn > div.prodListBtn-w > a");
-    await page.waitForSelector("div.prodListWrap");
-    const list = await page.$$("li.prod_list");
-    const prods = [];
-    for (let item of list) {
+    await page.goto("https://cu.bgfretail.com/product/pb.do?category=product&depth2=1&sf=N#");
+    await page.$eval("li.cardInfo_02 > a", e => e.click());
+    await page.$eval("#setC > a", e => e.click());
+    await new Promise((resolve) => { setTimeout(resolve, 1500) });
+    const cuList = await page.$$("li.prod_list");
+    const cuProds = [];
+    for (let item of cuList) {
         title = await item.$eval("div.prod_text > div.name > p", (e) => {
             return e.innerText;
         });
-        price = await item.$eval("div.prod_text > div.price > strong", (e)=> {
+        price = await item.$eval("div.prod_text > div.price > strong", (e) => {
             return e.innerText;
         });
-        imgsrc = await item.$eval("div.prod_img > img.prod_img", (e)=> {
+        imgsrc = await item.$eval("div.prod_img > img.prod_img", (e) => {
             return e.src;
         });
-        prods.push({ title: title, price: price, imgsrc: imgsrc});
+        cuProds.push({
+            title: title,
+            price: price,
+            imgsrc: imgsrc
+        });
     }
     await browser.close();
-    return prods;
+    return cuProds;
 }
 
 router.get("/", async (req, res) => {
