5c50e79ca67f7b65d21ffaaaabcf733fe7ae90b9
sloth-hub <madamhh@gmail.com>
2022-07-11 16:25:48 +0900

    Feat: 세븐일레븐, gs25 스크래핑 코드 추가

--- server/Router/scraper.js
+++ server/Router/scraper.js
@@ -11,39 +11,77 @@ async function scrapData() {
     ]);
     await Promise.all([
         page.goto("https://cu.bgfretail.com/product/pb.do?category=product&depth2=1&sf=N#"), // CU
-        page2.goto("https://www.7-eleven.co.kr/product/presentList.asp"), // 7-eleven
+        page2.goto("https://www.7-eleven.co.kr/product/bestdosirakList.asp"), // 7-eleven
         page3.goto("http://gs25.gsretail.com/gscvs/ko/products/youus-main"), // gs25
     ]);
 
     // CU
+
     await page.$eval("li.cardInfo_02 > a", e => e.click());
     await page.$eval("#setC > a", e => e.click());
     await new Promise((resolve) => { setTimeout(resolve, 1500) });
     const cuList = await page.$$("li.prod_list");
     const cuProds = [];
     for (let item of cuList) {
-        title = await item.$eval("div.prod_text > div.name > p", (e) => {
-            return e.innerText;
-        });
-        price = await item.$eval("div.prod_text > div.price > strong", (e) => {
-            return e.innerText;
-        });
-        imgsrc = await item.$eval("div.prod_img > img.prod_img", (e) => {
-            return e.src;
-        });
         cuProds.push({
-            title: title,
-            price: price,
-            imgsrc: imgsrc
+            title: await item.$eval("div.prod_text > div.name > p", (e) => {
+                return e.innerText;
+            }),
+            price: await item.$eval("div.prod_text > div.price > strong", (e) => {
+                return e.innerText;
+            }),
+            imgsrc: await item.$eval("div.prod_img > img.prod_img", (e) => {
+                return e.src;
+            })
         });
     }
+
+    // 7eleven
+
+    await page2.waitForSelector("div.dosirak_list");
+    const seList = await page2.$$("div.pic_product");
+    const seProds = [];
+    for (let item of seList) {
+        seProds.push({
+            title: await item.$eval("div.infowrap > div.name", (e) => {
+                return e.innerText;
+            }),
+            price: await item.$eval("div.infowrap > div.price > span", (e) => {
+                return e.innerText;
+            }),
+            imgsrc: await item.$eval("img", (e) => {
+                return e.src;
+            })
+        });
+    }
+
+    // gs25
+
+    await page3.waitForSelector("div.brdwrap");
+    const gsList = await page3.$$("div.prod_box");
+    const gsProds = [];
+
+    for (let item of gsList) {
+        gsProds.push({
+            title: await item.$eval("p.tit", (e) => {
+                return e.innerText;
+            }),
+            price: await item.$eval("p.price > span.cost", (e) => {
+                return e.innerText.slice(0, -1);
+            }),
+            imgsrc: await item.$eval("p.img > img", (e) => {
+                return e.src;
+            })
+        });
+    }
+
     await browser.close();
-    return cuProds;
+    return [cuProds, seProds, gsProds];
 }
 
 router.get("/", async (req, res) => {
-    const cudata = await scrapData();
-    res.send(cudata);
+    const prods = await scrapData();
+    res.send(prods);
 });
 
 module.exports = router;
