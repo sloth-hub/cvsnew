306644197b622e6f14be2e9c53567fb8848a547a
sloth-hub <madamhh@gmail.com>
2022-07-12 21:31:21 +0900

    Chore: hot prods 이미지 추가

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,21 +5,33 @@ const Home = ({ newProds }) => {
 
     return (
         <>
-            <ul className="prods">
-                {newProds.cu.slice(0, 8).map((newProd, index) =>
-                    <NewProds key={index} prods={newProd} />
-                )}
-            </ul>
-            <ul className="prods">
-                {newProds.se.slice(0, 8).map((newProd, index) =>
-                    <NewProds key={index} prods={newProd} />
-                )}
-            </ul>
-            <ul className="prods">
-                {newProds.gs.slice(0, 8).map((newProd, index) =>
-                    <NewProds key={index} prods={newProd} />
-                )}
-            </ul>
+            <div className="hot-prods">
+                <ul>
+                    <li>
+                        <img src="./images/hot_cu.jpg" alt="연세크림우유빵" />
+                    </li>
+                    <li>
+                        <img src="./images/hot_gs.jpg" alt="슈퍼말차라떼" />
+                    </li>
+                </ul>
+            </div>
+            <div className="new-prods">
+                <ul className="prods">
+                    {newProds.cu.slice(0, 8).map((newProd, index) =>
+                        <NewProds key={index} prods={newProd} />
+                    )}
+                </ul>
+                <ul className="prods">
+                    {newProds.se.slice(0, 8).map((newProd, index) =>
+                        <NewProds key={index} prods={newProd} />
+                    )}
+                </ul>
+                <ul className="prods">
+                    {newProds.gs.slice(0, 8).map((newProd, index) =>
+                        <NewProds key={index} prods={newProd} />
+                    )}
+                </ul>
+            </div>
         </>
     );
 }
