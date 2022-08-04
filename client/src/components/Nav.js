614e6ce5a980d0cb0e1d7e86c3f4249fb6f7937d
sloth-hub <madamhh@gmail.com>
2022-08-04 22:04:23 +0900

    Feat: 내비게이션 active css 적용

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
 
+    function activeMenu(e) {
+        const items = document.querySelectorAll("nav ul li");
+        items.forEach((e)=>{
+            e.classList.remove("active");
+        });
+        e.target.classList.add("active");
+    }
+
     return (
         <nav className="inner">
-                <Link to="/">
-                    <h1 className="logo">CVSNEW</h1>
-                </Link>
+            <Link to="/" onClick={activeMenu}>
+                <h1 className="logo">CVSNEW</h1>
+            </Link>
             <ul>
-                <Link to="/best">
+                <Link to="/best" onClick={activeMenu}>
                     <li>BEST</li>
                 </Link>
-                <Link to="/cu">
+                <Link to="/cu" onClick={activeMenu}>
                     <li>CU</li>
                 </Link>
-                <Link to="/se">
+                <Link to="/se" onClick={activeMenu}>
                     <li>7ELEVEN</li>
                 </Link>
-                <Link to="/gs">
+                <Link to="/gs" onClick={activeMenu}>
                     <li>GS25</li>
                 </Link>
             </ul>
