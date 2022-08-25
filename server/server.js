1b8f6600ab79434f774612b96448cefacbd5dff3
sloth-hub <madamhh@gmail.com>
2022-08-25 20:19:29 +0900

    Chore: server.js 수정

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const scraper = require("./Router/scraper");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => { console.log(`Listening on port ${port}`) });
 
 app.use("/all", scraper);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+});
