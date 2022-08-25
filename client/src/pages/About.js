f2e5031862c39c3117da52ee10786310528fee65
sloth-hub <madamhh@gmail.com>
2022-08-25 16:52:19 +0900

    Build: 헤로쿠 배포

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div className="best-wrap">
             <div className="inner">
-               
+               <p>3대 편의점 신상을 바로바로 확인할 수 있는</p>
             </div>
         </div>
     )
