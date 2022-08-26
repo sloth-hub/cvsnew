00846606657e3b4696c140af302650946c621e01
sloth-hub <madamhh@gmail.com>
2022-08-26 15:31:57 +0900

    Fix: 503 error 수정

--- server/server.js
+++ server/server.js
@@ -7,8 +7,9 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => { console.log(`Listening on port ${port}`) });
 
 app.use(express.static(path.join(__dirname, '../client/build')));
-app.use("/all", scraper);
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
 });
+
+app.use("/all", scraper);
