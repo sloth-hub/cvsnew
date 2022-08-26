9f226cb849212ad11c057d238e4ada430512bfc6
sloth-hub <madamhh@gmail.com>
2022-08-26 17:54:49 +0900

    Chore: cors 추가

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const cors = require("cors");
 const scraper = require("./Router/scraper");
 const port = process.env.PORT || 5000;
 
+app.use(cors);
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use("/all", scraper);
 
