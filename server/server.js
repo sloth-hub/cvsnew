bca152d33116711502361a985cf392334cd81e98
sloth-hub <madamhh@gmail.com>
2022-08-31 16:16:06 +0900

    Test: puppeteer 세븐일레븐 테스트

--- server/server.js
+++ server/server.js
@@ -25,83 +25,83 @@ async function scrapAll() {
             "--disable-setuid-sandbox"
         ]
     });
-    const [page, page2, page3, page4] = await Promise.all([
+    const [page2, page3] = await Promise.all([
+        // browser.newPage(),
         browser.newPage(),
-        // browser.newPage(),
-        // browser.newPage(),
+        browser.newPage(),
         // browser.newPage()
     ]);
 
     await Promise.all([
-        await page.setRequestInterception(true),
-        // await page2.setRequestInterception(true),
-        // await page3.setRequestInterception(true),
+        // await page.setRequestInterception(true),
+        await page2.setRequestInterception(true),
+        await page3.setRequestInterception(true),
         // await page4.setRequestInterception(true)
     ]);
 
     await Promise.all([
-        page.on('request', (req) => {
+        // page.on('request', (req) => {
+        //     speedUp(req);
+        // }),
+        page2.on('request', (req) => {
             speedUp(req);
         }),
-        // page2.on('request', (req) => {
-        //     speedUp(req);
-        // }),
-        // page3.on('request', (req) => {
-        //     speedUp(req);
-        // }),
+        page3.on('request', (req) => {
+            speedUp(req);
+        }),
         // page4.on('request', (req) => {
         //     speedUp(req);
         // })
     ]);
 
     await Promise.all([
-        page.goto("https://cu.bgfretail.com/product/pb.do?category=product&depth2=1&sf=N#"), // CU
-        // page2.goto("https://www.7-eleven.co.kr/product/bestdosirakList.asp"), // 7-eleven
-        // page3.goto("https://www.7-eleven.co.kr/product/7prodList.asp"), // 7-eleven
+        // page.goto("https://cu.bgfretail.com/product/pb.do?category=product&depth2=1&sf=N#"), // CU
+        page2.goto("https://www.7-eleven.co.kr/product/bestdosirakList.asp"), // 7-eleven
+        page3.goto("https://www.7-eleven.co.kr/product/7prodList.asp"), // 7-eleven
         // page4.goto("http://gs25.gsretail.com/gscvs/ko/products/youus-freshfood") // gs25
     ]);
 
     // CU
 
-    await Promise.all([
-        page.waitForNavigation({ waitUntil: "networkidle2" }),
-        page.$eval("li.cardInfo_02 > a", e => e.click()),
-        page.waitForNavigation({ waitUntil: "networkidle2" }),
-        page.$eval("#setC > a", e => e.click())
-    ]);
-    const cuList = await page.$$("li.prod_list");
-    const cuProds = [];
-    for (let item of cuList) {
-        cuProds.push({
-            title: await item.evaluate((e) => {
-                if (e.querySelector("div.tag > span.new")) {
-                    return e.querySelector("div.prod_text > div.name > p").innerText;
-                }
-            }),
-            price: await item.evaluate((e) => {
-                if (e.querySelector("div.tag > span.new")) {
-                    return e.querySelector("div.prod_text > div.price > strong").innerText;
-                }
-            }),
-            imgsrc: await item.evaluate((e) => {
-                if (e.querySelector("div.tag > span.new")) {
-                    return e.querySelector("div.prod_img > img.prod_img").src;
-                }
-            })
-        });
-    }
+    // await Promise.all([
+    //     page.waitForNavigation({ waitUntil: "networkidle2" }),
+    //     page.$eval("li.cardInfo_02 > a", e => e.click()),
+    //     page.waitForNavigation({ waitUntil: "networkidle2" }),
+    //     page.$eval("#setC > a", e => e.click())
+    // ]);
+    // const cuList = await page.$$("li.prod_list");
+    // const cuProds = [];
+    // for (let item of cuList) {
+    //     cuProds.push({
+    //         title: await item.evaluate((e) => {
+    //             if (e.querySelector("div.tag > span.new")) {
+    //                 return e.querySelector("div.prod_text > div.name > p").innerText;
+    //             }
+    //         }),
+    //         price: await item.evaluate((e) => {
+    //             if (e.querySelector("div.tag > span.new")) {
+    //                 return e.querySelector("div.prod_text > div.price > strong").innerText;
+    //             }
+    //         }),
+    //         imgsrc: await item.evaluate((e) => {
+    //             if (e.querySelector("div.tag > span.new")) {
+    //                 return e.querySelector("div.prod_img > img.prod_img").src;
+    //             }
+    //         })
+    //     });
+    // }
 
     // 7eleven
 
     const seProds = [];
-    // await page2.waitForSelector("div.dosirak_list");
-    // const seList1 = await page2.$$("div.dosirak_list > ul >li:not(:first-child):not(:last-child)");
-    // getSE(seList1, seProds);
+    await page2.waitForSelector("div.dosirak_list");
+    const seList1 = await page2.$$("div.dosirak_list > ul >li:not(:first-child):not(:last-child)");
+    getSE(seList1, seProds);
 
-    // await page3.$eval("ul.tab_layer > li:nth-child(2) > a", e => e.click());
-    // await page3.waitForSelector("#listUl");
-    // const seList2 = await page3.$$("#listUl >li:not(:first-child):not(:last-child)");
-    // getSE(seList2, seProds);
+    await page3.$eval("ul.tab_layer > li:nth-child(2) > a", e => e.click());
+    await page3.waitForSelector("#listUl");
+    const seList2 = await page3.$$("#listUl >li:not(:first-child):not(:last-child)");
+    getSE(seList2, seProds);
 
     // gs25
 
@@ -134,7 +134,7 @@ async function scrapAll() {
     // }
 
     await browser.close();
-    return [cuProds, seProds, gsProds];
+    return [seProds];
 }
 
 function speedUp(req) {
