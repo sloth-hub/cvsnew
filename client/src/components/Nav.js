ce02176b5edad83fcf3b818b062cb429a8e7826d
sloth-hub <madamhh@gmail.com>
2022-09-20 20:59:31 +0900

    Design: 햄버거 메뉴 css 추가

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,14 +1,19 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { FiMenu } from "react-icons/fi"
 
 const Nav = () => {
 
     function activeMenu(e) {
         const items = document.querySelectorAll("nav ul li");
-        items.forEach((e)=>{
+        items.forEach((e) => {
             e.classList.remove("active");
         });
         e.target.classList.add("active");
+    }
+
+    function hmbgrMenu(e) {
+        document.querySelector("ul.menu").classList.toggle("active");
     }
 
     return (
@@ -16,7 +21,7 @@ const Nav = () => {
             <Link to="/" onClick={activeMenu}>
                 <h1 className="logo">CVSNEW</h1>
             </Link>
-            <ul>
+            <ul className="menu">
                 <Link to="/about" onClick={activeMenu}>
                     <li>ABOUT</li>
                 </Link>
@@ -30,6 +35,10 @@ const Nav = () => {
                     <li>GS25</li>
                 </Link>
             </ul>
+            <button className="hmbgr" onClick={hmbgrMenu}>
+                <FiMenu size="30" color="#5b5b5d" />
+                <div className="blind">menu</div>
+            </button>
         </nav>
     )
 }
