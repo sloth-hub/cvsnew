bc43d9773f341b74dbc2dac25fbd0502fd710530
sloth-hub <madamhh@gmail.com>
2022-10-20 20:43:47 +0900

    Test: db 업데이트 테스트

--- server/server.js
+++ server/server.js
@@ -6,16 +6,16 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 const admin = require("firebase-admin");
 const { chromium } = require("playwright");
-var serviceAccount = require(path.resolve(__dirname, '../serviceAccountKey.json'));
+// var serviceAccount = require(path.resolve(__dirname, '../serviceAccountKey.json'));
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://cvsnew-a3611-default-rtdb.firebaseio.com"
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://cvsnew-a3611-default-rtdb.firebaseio.com"
+// });
 
-var db = admin.database();
+// var db = admin.database();
 
 app.get("/update", async (req, res) => {
     const [data1, data2] = await Promise.all([
