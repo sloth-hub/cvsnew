79f95afaf6607a8e554e453b403baf0640f8f43e
sloth-hub <madamhh@gmail.com>
2022-10-31 20:38:16 +0900

    Design: 행사상품 페이지 기본 레이아웃 추가

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,7 +1,28 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { database } from "../firebase";
+import { get, ref, child } from "firebase/database";
 
 const Events = () => {
+
+    const [evtProds, setEvtProds] = useState(null);
+
+    useEffect(() => {
+        const dbRef = ref(database);
+        get(child(dbRef, "prods")).then((snapshot) => {
+            const data = snapshot.val();
+            let cuData = Object.values(data.cu);
+            const gsData = Object.values(data.gs);
+            const seData = Object.values(data.se);
+            setEvtProds({
+                cu: cuData,
+                se: seData,
+                gs: gsData
+            });
+        }).catch((err) => {
+            console.log(err);
+        });
+    }, []);
 
     function test() {
         axios.get("update").then((res) => {
@@ -15,11 +36,32 @@ const Events = () => {
         });
     }
 
+    function clickedTab(e) {
+        e.preventDefault();
+    }
+
     return (
         <div className="events-wrap">
             <div className="inner">
-                <button onClick={test}>update</button>
-                <button onClick={evt}>events</button>
+                <h2>행사상품</h2>
+                <div className="tab-wrap">
+                    <ul className="main-tab">
+                        <li><a href="#" onClick={clickedTab}>전체</a></li>
+                        <li><a href="#" onClick={clickedTab}>CU</a></li>
+                        <li><a href="#" onClick={clickedTab}>7-ELEVEN</a></li>
+                        <li><a href="#" onClick={clickedTab}>GS25</a></li>
+                    </ul>
+                    <ul className="sub-tab">
+                        <li><a href="#" onClick={clickedTab}>전체</a></li>
+                        <li><a href="#" onClick={clickedTab}>1+1</a></li>
+                        <li><a href="#" onClick={clickedTab}>2+1</a></li>
+                        <li><a href="#" onClick={clickedTab}>증정</a></li>
+                        <li><a href="#" onClick={clickedTab}>할인</a></li>
+                    </ul>
+                </div>
+                <div className="prods">
+
+                </div>
             </div>
         </div>
     )
