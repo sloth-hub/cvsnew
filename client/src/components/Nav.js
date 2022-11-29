c48f3d1412fdfd9f04e0c36b46cf22acf734745a
sloth-hub <madamhh@gmail.com>
2022-11-29 21:38:47 +0900

    Chore: path에 따른 내비게이션 메뉴 active 토글 코드 추가

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FiMenu } from "react-icons/fi"
 
 const Nav = () => {
+
+    useEffect(() => {
+        menuInit();
+    }, []);
+
+    function menuInit() {
+        let path = window.location.pathname.substr(1);
+        const items = document.querySelectorAll("nav ul li");
+        items.forEach((e) => {
+            if (e.dataset.value === path) {
+                e.classList.add("active");
+            }
+        });
+    }
 
     function activeMenu(e) {
         const items = document.querySelectorAll("nav ul li");
@@ -23,16 +37,16 @@ const Nav = () => {
             </Link>
             <ul className="menu">
                 <Link to="/events" onClick={activeMenu}>
-                    <li>EVENTS</li>
+                    <li data-value="events">EVENTS</li>
                 </Link>
                 <Link to="/cu" onClick={activeMenu}>
-                    <li>CU</li>
+                    <li data-value="cu">CU</li>
                 </Link>
                 <Link to="/se" onClick={activeMenu}>
-                    <li>7ELEVEN</li>
+                    <li data-value="se">7ELEVEN</li>
                 </Link>
                 <Link to="/gs" onClick={activeMenu}>
-                    <li>GS25</li>
+                    <li data-value="gs">GS25</li>
                 </Link>
             </ul>
             <button className="hmbgr" onClick={hmbgrMenu}>
