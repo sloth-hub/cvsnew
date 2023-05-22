8d2caca8d709fb15b3512e0ff1c4b26f727e700d
sloth-hub <madamhh@gmail.com>
2023-05-22 22:30:10 +0900

    Chore: 슬라이드 이미지 추가

--- client/src/components/Slide.js
+++ client/src/components/Slide.js
@@ -7,47 +7,39 @@ const Slide = () => {
     const settings = {
         dots: true,
         infinite: true,
-        autoplay: true,
-        autoplaySpeed: 4000,
+        autoplay: false,
+        // autoplaySpeed: 4000,
         speed: 1300,
         arrows: false
     }
 
-    useEffect(() => {
-        window.onresize = (e) => {
-            const textbox = document.querySelectorAll("div.text-box");
-            textbox.forEach((e) => {
-                if (window.innerWidth <= 767) {
-                    e.classList.remove("blind");
-                } else {
-                    e.classList.add("blind");
-                }
-            });
-        }
-    }, []);
-
     return (
         <Slider {...settings}>
             <div className="slide-box one">
-                <div className="img-box"></div>
-                <div className={window.innerWidth <= 767 ? "text-box" : "text-box blind"}>
-                    <p>팥·완두앙금과 버터크림,
-                         국산 딸기잼을 듬뿍 넣은</p>
-                    <h3>CU 고대 1905<br/>딸기잼 맘모스빵</h3>
+                <div className="img-box">
+                    <img src={`${process.env.PUBLIC_URL}/images/bg_1_s.png`} />
+                </div>
+                <div className="text-box blind">
+                    <p>부드러운 말차크림을 진한 커스타드와 화이트 초콜렛으로 감싼  </p>
+                    <h3>CU 연세우유 말차생크림빵</h3>
                 </div>
             </div>
             <div className="slide-box two">
-                <div className="img-box"></div>
-                <div className={window.innerWidth <= 767 ? "text-box" : "text-box blind"}>
+                <div className="img-box">
+                <img src={`${process.env.PUBLIC_URL}/images/bg_2_s.png`}/>
+                </div>
+                <div className="text-box blind">
                     <p>물대신 제주우유로 반죽해서 더 부드러운</p>
-                    <h3><span>7</span>-Eleven<br />제주우유 생크림빵</h3>
+                    <h3>7-Eleven 제주우유 생크림빵</h3>
                 </div>
             </div>
             <div className="slide-box three">
-                <div className="img-box"></div>
-                <div className={window.innerWidth <= 767 ? "text-box" : "text-box blind"}>
+                <div className="img-box">
+                <img src={`${process.env.PUBLIC_URL}/images/bg_3_s.png`} />
+                </div>
+                <div className="text-box blind">
                     <p>상큼 딸기 X 촉촉 생크림 X 깜찍 몰랑이</p>
-                    <h3>GS25 딸기<br />샌드위치 에디션</h3>
+                    <h3>GS25 딸기 샌드위치 에디션</h3>
                 </div>
             </div>
         </Slider>
