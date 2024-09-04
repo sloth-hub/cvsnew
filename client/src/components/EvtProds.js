8affba136820badd5515994d4267e4e6c07ba0ce
sloth-hub <madamhh@gmail.com>
2024-09-04 21:55:37 +0900

    Chore: 증정품 표시 마우스오버 아이콘 추가

--- client/src/components/EvtProds.js
+++ client/src/components/EvtProds.js
@@ -15,14 +15,19 @@ const EvtProds = ({ prods, isHome, uid }) => {
             <div className="img-loader"></div>
             <div className="img-box">
                 <img src={prods.imgsrc.indexOf("http") === -1 ? "images/error.webp" : prods.imgsrc} alt={prods.title} onLoad={imgLazyLoading} />
+                {prods.title.includes("증정") &&
+                    <div className="giveaway-wrap">
+                        <span className="giveaway">증정품</span>
+                        <span className="giveaway-name">{prods.title.split("증정")[1].substr(1)}</span>
+                    </div>}
             </div>
             <div className="ico">
-                <i className="evt-type">{prods.type}</i>
+                {prods.type !== "증정" && <i className="evt-type">{prods.type}</i>}
                 <i className={`evt-store ${prods.store === "7-eleven" ? "se" : prods.store}`}>{prods.store === "emart24" ? "이마트24" : prods.store}</i>
             </div>
-            {isHome ? null : <Buttons prods={prods} cvs={prods.store} uid={uid} />}
             <div className="info">
-                <h3>{prods.title}</h3>
+                {isHome ? null : <Buttons prods={prods} cvs={prods.store} uid={uid} />}
+                <h3>{prods.title.includes("증정") ? prods.title.split("증정")[0].slice(0, -2) : prods.title}</h3>
                 {prods.price.discount ?
                     <p><strong>{prods.price.discount}</strong>원<del className="discount">{prods.price.cost}</del></p> :
                     <p><strong>{prods.price}</strong>원</p>
